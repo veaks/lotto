f83d3bc7cf053fc74039fa96d755e6753463e5ef
Val <veaks@yandex.ru>
2022-03-28 12:38:53 +0300

    repaire 5

--- lotto/urls.py
+++ lotto/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from lotto.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stats.urls')),
+    path('index/', index),
 ]
