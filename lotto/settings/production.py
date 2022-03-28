b55f86761a83ded88dd9bfd59e7628de2c89e6a6
Val <veaks@yandex.ru>
2022-03-28 12:45:05 +0300

    repaire 7

--- lotto/settings/production.py
+++ lotto/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-%+leiiuz1j%bh_b!b+%h^7x)4uwtuw471!*rul_)jh#e%4!5%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
