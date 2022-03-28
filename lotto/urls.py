081a14bb5984348aeb95f30fd5f110b58b3a9f2a
Val <veaks@yandex.ru>
2022-03-28 13:47:23 +0300

    repaire 12

--- lotto/urls.py
+++ lotto/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+import stats
 from lotto.views import index
+from stats.views import list_draws, overall_stat, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', include('stats.urls')),
 ]
--- stats/urls.py
+++ stats/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from lotto.views import index
 from stats.views import list_draws, overall_stat, update
 
 urlpatterns = [
-    path('list_draws/', list_draws, name='list_draws'),
-    path('overall_stat/', overall_stat, name='overall_stat'),
-    path('update/', update, name='update'),
+    path('list_draws/', list_draws),
+    path('overall_stat/', overall_stat),
+    path('update/', update),
+    path('', index),
 ]
