20f79137585933ecdd6ae3b7b3ef7fceb0aa55ad
Val <veaks@yandex.ru>
2022-03-28 11:10:56 +0300

    repaire 1

--- lotto/settings/production.py
+++ lotto/settings/production.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
